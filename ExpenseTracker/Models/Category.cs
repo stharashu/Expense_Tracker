8a2b5fbd1a7d4f7530a438f130e3d5de359eee93
stharashu <shrestha.rashu1@gmail.com>
2023-12-25 09:45:34 +0545

    Add project files.

---
+++ ExpenseTracker/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
+    }
+}
